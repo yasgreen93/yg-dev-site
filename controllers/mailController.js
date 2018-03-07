6979003efce284a84c89cd17ba50604153e0b326
Yasmin Green <yasmin.green@btinternet.com>
2018-03-07 09:31:01 +0000

    trying updating session flash

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -14,7 +14,8 @@ const mail = require('../handlers/mail');
 // }
 exports.sendEmail = (req, res, next) => {
 	const { name, email, subject, message } = req.body;
-	req.flash('success', 'Thank you for getting in contact!');
+	// req.flash('success', 'Thank you for getting in contact!');
+	req.session.flash = { success: ['Thank you for getting in contact!'] };
 	console.log('--sessionOptions before redirect', req.session);
 	res.redirect('/contact')
 }
