b75b3054cd8bc30b96f1479ff28f6535c7156f20
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2018-03-18 18:54:23 +0000

    improving error page

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -69,5 +69,7 @@ exports.developmentErrors = (err, req, res, next) => {
 */
 exports.productionErrors = (err, req, res, next) => {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    status: err.status,
+  });
 };
